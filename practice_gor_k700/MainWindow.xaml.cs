6c74970bc1cb9c6e6fb9a0a7558c2d9f0fc547a5
arbuz <arbuz@LAPTOP-MMVS2P4B>
2022-06-11 11:39:15 +0800

    Добавьте файлы проекта.

---
+++ practice_gor_k700/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace practice_gor_k700
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        prac_gotEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new prac_gotEntities();
+        }
+
+        private void EnterInSys(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int tabNam = Convert.ToInt32(LoginWorker.Text);
+                string pass = PasswordWorker.Text;
+
+                Worker worker = context.Worker.ToList().Find(x => x.tabNum == tabNam);
+                if (worker == null)
+                {
+                    LoginWorker.Background = new SolidColorBrush(Colors.Red);
+                    PasswordWorker.Background = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("Такого логина не существует, попробуйте другой!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    if (worker.password.Equals(pass))
+                    {
+                        LoginWorker.Background = new SolidColorBrush(Colors.Green);
+                        PasswordWorker.Background = new SolidColorBrush(Colors.Green);
+                        MessageBox.Show("Пароль верный! Выполняется вход!", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Window1 window1 = new Window1();
+                        window1.Show();
+                    }
+                    else
+                    {
+                        PasswordWorker.Background = new SolidColorBrush(Colors.Red);
+                        MessageBox.Show("Неправильно введен пароль!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                LoginWorker.Background = new SolidColorBrush(Colors.Red);
+                PasswordWorker.Background = new SolidColorBrush(Colors.Red);
+                MessageBox.Show("Неверно введен логин!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+
+        
+
+        }
+    }
+}
---
+++ practice_gor_k700/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practice_gor_k700
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        prac_gotEntities context;
+        public Window1()
+        {
+            InitializeComponent();
+            context = new prac_gotEntities();
+            //NameOfWorker = context.Worker.ToList().Find(FIO);
+        }
+
+        private void Watch(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
